b924ff3053a98f57ffaeddedb740aa99c674b043
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2024-05-30 14:12:03 +0530

    "second commit"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({setShowLogin}) => {
     const [menu,setMenu]=useState('Home')
+    const {getTotalCartAmount}=useContext(StoreContext)
   return (
     <div className='navbar'>
+      <Link to="/">
       <img src={assets.logo} alt="" className='logo'/>
+      </Link>
+      
       <ul className="navbar-menu">
         <Link  to="/" className={menu==="Home"?"active":""} onClick={()=>setMenu("Home")}>Home</Link>
         <a href="#explore-menu" className={menu==="Menu"?"active":""} onClick={()=>setMenu("Menu")}>Menu</a>
@@ -18,8 +23,9 @@ const Navbar = ({setShowLogin}) => {
       <div className='navbar-right'>
         <img src={assets.search_icon} alt=""/>
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt=""/>
-          <div className='dot'></div>
+          <NavLink to="/cart"> <img src={assets.basket_icon} alt=""/></NavLink>
+         
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>SignIn</button>
       </div>
