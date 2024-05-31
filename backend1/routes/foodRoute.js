19735980453f264148926f20f0b987aa3739e0ea
rhemanth <rhemanth399@gmail.com>
2024-05-31 18:35:59 +0530

    "update1"

--- backend1/routes/foodRoute.js
+++ backend1/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addFood, listFood } from '../../backend1/controllers/foodController.js'
+import { addFood, listFood , removeFood } from '../controllers/foodController.js'
 import multer from 'multer'
 
 const foodRouter= express.Router();
@@ -21,6 +21,9 @@ const upload=multer({storage:storage})
 foodRouter.post("/add",upload.single("image"),addFood)
 
 foodRouter.get("/list",listFood)
+
+foodRouter.post("/remove",removeFood)
+
 
 
 
