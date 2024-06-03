153c26c4eb73a6f25026cf0b32fbbd73036713cc
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2024-06-03 21:45:06 +0530

    "frontend added"

--- backend1/controllers/orderController.js
+++ backend1/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe= new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user from frontend
 const placeOrder= async(req,res)=>{
-    const frontend_url= "http://localhost:5174"
+    const frontend_url= process.env.FRONT_END
 
     try {
         const newOrder= new orderModel({
